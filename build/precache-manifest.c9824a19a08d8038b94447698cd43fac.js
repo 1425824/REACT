8063569c733c1021d11b394ed768e6f10a7654e1
joelfeca <Joel.FelipC@e-campus.uab.cat>
2020-04-28 19:09:07 +0200

    pwa

---
+++ build/precache-manifest.c9824a19a08d8038b94447698cd43fac.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a331f54548d23b06204d0a89d599ab45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f091fa8ad12a2627390",
+    "url": "/static/css/2.74c45cc7.chunk.css"
+  },
+  {
+    "revision": "2e1e7d343a9a099aebdf",
+    "url": "/static/css/main.cc45baed.chunk.css"
+  },
+  {
+    "revision": "4f091fa8ad12a2627390",
+    "url": "/static/js/2.41753ecf.chunk.js"
+  },
+  {
+    "revision": "8367444f61037511e6bf4d0fb46d56ab",
+    "url": "/static/js/2.41753ecf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1e7d343a9a099aebdf",
+    "url": "/static/js/main.21ccd674.chunk.js"
+  },
+  {
+    "revision": "ab8c640a5c042e8a715b",
+    "url": "/static/js/runtime-main.1055f5cb.js"
+  },
+  {
+    "revision": "742ab6da31fd0a84307d61d360d812c1",
+    "url": "/static/media/car1.742ab6da.png"
+  },
+  {
+    "revision": "3e5fbc8266d89183b60146c153f42c8d",
+    "url": "/static/media/car2.3e5fbc82.png"
+  },
+  {
+    "revision": "41d884034c9c9c9818aec92498b8e870",
+    "url": "/static/media/car3.41d88403.png"
+  },
+  {
+    "revision": "1dbade5bcb93499e5c1f06c4ef8bd248",
+    "url": "/static/media/draw.1dbade5b.png"
+  },
+  {
+    "revision": "327c04ce53e0b67cb0e4cfca77bcb0d9",
+    "url": "/static/media/ej icon.327c04ce.png"
+  },
+  {
+    "revision": "c7733751ae92a19950799225f35b45c5",
+    "url": "/static/media/informes.c7733751.png"
+  },
+  {
+    "revision": "928416ac64e91dd147514e239f3d1a07",
+    "url": "/static/media/itennis.928416ac.png"
+  },
+  {
+    "revision": "1b0c69b59410a7a4d7cfff94882f4959",
+    "url": "/static/media/pissarra1.1b0c69b5.jpg"
+  },
+  {
+    "revision": "8df8f64e44c449c49595f951ade9e729",
+    "url": "/static/media/pissarra2.8df8f64e.jpg"
+  },
+  {
+    "revision": "12342aced44e35a5de291a57fc323be4",
+    "url": "/static/media/pissarra3.12342ace.jpg"
+  },
+  {
+    "revision": "13deb6f222128b3fbe7d2862149532ac",
+    "url": "/static/media/test.13deb6f2.png"
+  }
+]);
